619cf60c2827c5e3376d7bab1dd5098863a67ad4
Prateek-Shetty <prateekshetty2552@gmail.com>
2025-06-03 15:51:04 +0530

    Nav and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import Nav from './components/Nav';
 import './App.css';
+import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<h1>Products Page</h1>} />
+        <Route path="/add" element={<h1>Add Products Page</h1>} />
+        <Route path="/update" element={<h1>Update Products Page</h1>} />
+        <Route path="/logout" element={<h1>Logout Page</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+
+      
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+const Nav = () => {
+    return(
+        <>
+        <ul className="navul"> 
+            <li><Link to="/">Products</Link> </li>
+            <li><Link to="/add">Add Products</Link> </li>
+            <li><Link to="/update">Update Products</Link> </li>
+            <li><Link to="/logout">log out</Link> </li>
+            <li><Link to="/profile">profile</Link> </li>
+
+        </ul>
+        </> 
+    )
+}
+
+export default Nav;
